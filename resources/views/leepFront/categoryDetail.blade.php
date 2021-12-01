4713aa5c8195302ca404acbafd810e6872e827ec
muzammalrahim <creative.joomdev@gmail.com>
2021-12-01 18:00:05 +0500

    category detail page changings

--- resources/views/leepFront/categoryDetail.blade.php
+++ resources/views/leepFront/categoryDetail.blade.php
@@ -9,71 +9,57 @@
 
         <div class="row">
 
-            <div class="col-md-2"> 
+            <div class="col-md-3 col-lg-2"> 
                 @include('layout.leftSidebar')
             </div>
 
             <div class="col-md-8">
                 <div class="events mx-3">
-                   <div class="search-title-box">
-
+                    <div class="search-title-box">
                         <div class="row">
-                            <div class="col-md-8">
-                                <h3 class="font-weight-bold"> Events in {{ $category->name }}</h3>
+                            <div class="col-md-12">
+                                
+                                <h1 class="mb-lg-3" style="font-family: 'impact';"> Events in {{ $category->name }}</h1>
+                                <div class="pl-lg-5 ml-lg-4 pl-md-5 ml-md-4">
+                                    <p class="">For the current year, this category includes: <b> {{ $eventCount }} </b> events.
+                                    </p>
+                                    <h4 class="mb-4 font-color font-weight-bold">
+                                        The Africa category highlights events that take place in the nations of Africa, or have a tie to Africa.
+                                    </h4>
+                                    <p class="mb-4">Africa is defined as:</p>
+                                    <p class="">
+                                        <b>Africa Northern Hemisphere:</b> Algeria, Benin, Burkina Faso, Cabo Verde, Canary Islands, Cameroon, Central African Republic, Chad, Cote d'Ivoire, Djibouti, Egypt, Equatorial Guinea, Eritrea, Ethiopia, Gabon, Ghana, Guinea, Guinea-Bissau, Liberia, Libya, Mali, Mauritania, Morocco, Niger, Nigeria, Republic of Congo, Senegal, Sierra Leon, Somalia, South Sudan, Sudan, The Gambia, Togo, Tunisia, Western Sahara.
+                                    </p>
+                                    <p class="mb-4">
+                                        <b>Africa Southern Hemisphere:</b> Angola, Botswana, Burundi, Comoros, Democratic Republic of Congo, Eswatini, Gabon, Kenya, Lesotho, Madagascar, Malawi, Mauritius, Mayotte, Mozambique, Namibia, Reunion, Rwanda, Sao Tome and Principe, Seychelles, South Africa, Tanzania, Uganda, Zambia, Zimbabwe.
+                                    </p>
+                                    <p class="mb-4 text-center">
+                                        A partial list of events will be displayed in the search results for<br> non-members. To view the full results, login to your LEEP<br> Membership account and search this category again, (requires<br> paid subscription).
+                                    </p>
+                                </div>
                             </div>
 
-                            <div class="col-md-4 text-right">
-                                <h6>{{$eventCategory->count()}} Results</h6>
-                            </div> 
+                            <div class="container mt-5">
+                                <div class="row pl-lg-5 ml-lg-5 pl-md-0 ml-md-0">
+                                    <div class="col-4">
+                                        <a href="{{route('advance.search')}}" class="custom_btn btn rounded-0 font-weight-bold px-3 px-lg-5 px-md-5 px-sm-5 py-3 text-white bg_maroon" type="submit">Search</a>
+                                    </div>
+                                    <div class="col-4">
+                                        <a href="{{URL::asset('login')}}" class="custom_btn btn btn-outline-secondary rounded-0 text-white font-weight-bold px-3 px-lg-5 px-md-5 px-sm-5 py-3 bg_light"  type="submit">Login</a>
+                                    </div>
+                                    <div class="col-4">
+                                        <a href="{{URL::asset('register')}}" class="custom_btn btn rounded-0 font-weight-bold px-3 px-lg-5 px-md-5 px-sm-5 py-3 text-white bg_green" type="submit">Register</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="event-box">
-
-                        
-                        @foreach($eventCategory as $e)
-                            {{-- @dump($e->id) --}}
-        	               <a href=" {{ route('eventDetail',['id'=>$e->event_id]) }} ">
-                            <div class="event-title" style=" cursor:pointer;">
-        	                    <div class="row">	                    	
-        		                        <div class="col-md-10 categoryEvent">
-        		                            <h4>{{\Illuminate\Support\Str::limit($e->event->name,42)}}</h4>
-        		                            <p class="event-type">{{$e->event->type}}</p>
-        		                            <p>{{\Illuminate\Support\Str::limit($e->event->description,204)}}</p>
-
-        		                        </div>
-        		                        <div class="col-md-2" style="padding-left: 0px; text-align: center;">
-        		                            <h5 class="date-year">{{$e->event->d_start}} {{date("F", strtotime(date("Y")."-".$e->event->m_start."-01"))}}</h5>
-                                            <label class = "labelclass">{{$e->event->status}}</label>
-        		                        </div>	                    	
-        	                    </div>
-        	                </div>
-                            </a>
-                        @endforeach
-                    </div>
-
-                    <div class="borderline my-3"></div>
-
-
-                    <div class="w3-center w3-bar w3-border w3-round" style="text-align: center;">
-                        @if (Auth::check() && hasMemberShip())
-                            {{-- expr --}}
-                         {{$eventCategory->appends(Request::except('page'))->links('pagination::default') }}
-
-                        @endif
-                     </div>
-
                 </div>
             </div>
-            
-            <div class="col-md-2">
-                @include('layout.rightSidebar')
-            </div>
-
         </div>
-
-                    {{-- <div class="borderline my-3"></div> --}}
-
     </div>
-
 @endsection
+<style type="text/css">
+    p{
+        color: #000000 !important;
+    }
+</style>
