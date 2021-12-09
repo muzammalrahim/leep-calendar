e683aa864493a0c39e2a2774a3e8763e4d007a66
muzammalrahim <creative.joomdev@gmail.com>
2021-12-09 18:43:33 +0500

    event detail page

---
+++ resources/views/leepFront/eventDetail/championInformation.blade.php
@@ -0,0 +1,120 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="event-champion">
+            <h4 class="font-weight-bold bg_black text-light p-2 pl-4 m-0"> EVENT CHAMPION </h4>
+        </div>
+
+        <div class="focal-contact1 m-0 bg_lightyellow">
+            <div class="row m-0 pt-5 pl-5 pr-5">
+                <div class="col-12 m-0">
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> EVENT CHAMPION: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> Prevent Blindness America </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> ADDRESS 1: </span>
+                        </div>
+                        <div class="">
+                            <span class="ml-3 font18"> 211 West Wacker Drive </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> ADDRESS 2: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> Suite 1700 </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row m-0 pl-5 pr-5">
+                <div class="col-sm-12 col-md-12 col-lg-6 col-12">
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> CITY: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> Chicago </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> STATE: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> IL </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> COUNTRY: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> US </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> ZIP CODE: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> 60606 </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-6 col-12">
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> PHONE: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> (800) 331.2020 </span>
+                        </div>
+                    </div>
+
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> EMAIL: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18  text_marron"> info@preventblindness.org </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> CONTACT: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18"> General Information </span>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="float-left">
+                            <span class="font-weight-bold font20"> CONTACT URL: </span>
+                        </div>
+                        <div>
+                            <span class="ml-3 font18 text_marron"> https://www.preventblind-ness.org/contact-us </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- <div class="row m-0">
+                <div class="focal-person">
+                    <div class="col-md-4 ">
+                        <img src="{{URL::asset('leep_calender/images/Developer Assets/Event View Page/Rectangle 256.svg')}}"
+                        alt="">
+                    </div>
+                    <div class="col-md-8 ser-name">
+                        <a href="{{ route('champion.detail',['id' => $event->id]) }}" target="_blank" class="eventChampionLink font-weight-bold">{{ $event->event_champion }}</a>
+                    </div>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+</div>
